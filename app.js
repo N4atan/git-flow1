d3e65cb00b08b84acc1efaaa15024366bf36a698
NATAN GABRIEL WILDNER DA SILVA <02526886007@senacrs.edu.br>
2024-11-26 20:15:24 -0300

    Melhorias no design da interface para adicionar tarefas. Agora é possível visualizar as tarefas adicionadas.

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
-let tarefas = [];
+    let tarefas = [];
 
-document.getElementById("meu-formulario").addEventListener("submit", (evento) => {
-    evento.preventDefault();
+    document.getElementById("meu-formulario").addEventListener("submit", (evento) => {
+        evento.preventDefault();
 
-    let tarefa = document.getElementById("task").value;
-    let date = document.getElementById("date").value.split('-').reverse().join('-');
+        let tarefa = document.getElementById("task").value;
+        let date = document.getElementById("date").value.split('-').reverse().join('-');
 
-    if(tarefa == "" && date == "") {
-        alert("Adicione uma tarefa primeiro!");
-        
-    } else {
-        let task = {
-            tarefa: tarefa,
-            date: date
+        if(tarefa == "" && date == "") {
+            alert("Adicione uma tarefa primeiro!");
+            
+        } else {
+            let task = {
+                tarefa: tarefa,
+                date: date
+            }
+
+            console.log(task);
+            tarefas.push(task);
+
+            // Cria uma nova div para exibir a tarefa
+        const tarefaDiv = document.createElement("div");
+        tarefaDiv.classList.add("tarefa");
+
+        // Adiciona o conteúdo na div
+        tarefaDiv.innerHTML = `
+            <p><strong>Tarefa:</strong> ${task.tarefa}</p>
+            <p><strong>Data:</strong> ${task.date}</p>
+        `;
+
+        // Adiciona a div ao corpo da página
+        document.getElementById('container-div').appendChild(tarefaDiv);
+
+            document.getElementById("task").value = "";
+            document.getElementById("date").value = "";
         }
-
-        console.log(task);
-        tarefas.push(task);
-
-        document.getElementById("task").value = "";
-        document.getElementById("date").value = "";
-    }
-})
+    })
